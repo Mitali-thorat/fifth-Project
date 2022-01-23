08dd0a6757acb1f9280f2d3fa83b4bb206aeb5fd
mital <mital@DESKTOP-PQQKCMJ>
2022-01-23 18:13:12 +0530

    fifth commit

---
+++ 11-01-2022/src/com/Veriables/Example1.java
@@ -0,0 +1,36 @@
+package com.Veriables; // identifire (com.Veriables) is package
+
+public class Example1 // identifire is class
+{
+
+	public static void main(String[] args) /*  predefine verible is  public static 
+	 *void main(String[] args)*/
+	// identifire is main (method)
+	{
+		String mobileName = "xiaomi"; // verible is name
+		String mobileColour = "Rose gold"; // verible is colour
+		float mobilePrice = 4500; // verible is price
+		float mobileRam = 4; // verible is ram
+		float mobileRom = 64; // verible is rom
+		float mobileVersion= 9; // verible is version
+		float mobileIMEINo = 862245815; // verible is IMEI
+		boolean mobileQuality = true;// true means good and false means bad 
+		// verible is quality
+
+		System.out.println(mobileName);
+		System.out.println(mobileColour);
+		System.out.println(mobilePrice);
+		System.out.println(mobileRam);
+		System.out.println(mobileRom);
+		System.out.println(mobileVersion);
+		System.out.println(mobileIMEINo);
+		System.out.println(mobileQuality);
+
+	}
+
+
+
+
+}
+
+
